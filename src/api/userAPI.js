7a3816cb8d7a2266974bc853597c60c5e77aa0ea
Ravindra Sadaphule <rsadaphule@gmail.com>
2017-04-08 20:11:50 -0700

    added use api

---
+++ src/api/userAPI.js
@@ -0,0 +1,23 @@
+import 'whatwg-fetch';
+
+
+export function getUsers()
+{
+  return get('users');
+}
+
+function get(url){
+
+  return fetch(url).then(onSuccess, onError);
+  
+}
+
+function onSuccess(response)
+{
+   return response.json();
+}
+
+function onError(error)
+{
+  console.og(error);
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-import numeral from 'numeral';
-import './index.css';
 
-const courseValue = numeral(1000).format('$0.0.000');
+import {getUsers} from './api/userAPI';
 
-console.log(`I would pay ${courseValue} for this awesome course!`); //eslint-disable-line no-console
-console.log("hello")
+
+getUsers().then(result => {
+    let usersBody = "";
+
+     result.forEach(user => {
+
+      usersBody += `<tr>
+                        <td><a href="#" data-id="${user.id}">Delete</a></td>
+                        <td>${user.id}</td>
+                        <td>${user.firstName}</td>
+                        <td>${user.lastName}</td>
+                        <td>${user.email}</td>
+                     </tr>`;
+     });
+
+
+      global.document.getElementById('users').innerHTML = usersBody;
+
+ 
+});
